6c9683d982b33194f949646dc8cefb308636a4e5
Logan Braga <braga.logan@gmail.com>
2016-06-25 23:57:11 +0200

    Improve pushmenu directive & style

--- app/components/base/pushMenu.js
+++ app/components/base/pushMenu.js
@@ -9,26 +9,38 @@ angular.module('loganbraga')
     link: function($scope, $element, $attr) {
       $element.on('click', function() {
         var menu = angular.element(document.querySelectorAll($attr.targetMenu));
+        var content = angular.element(document.getElementById('content'));
+
+        content.on('click', function() {
+          var isOpen = $element.hasClass('active');
+          if (isOpen) {
+            $element.removeClass('active');
+            menu.removeClass('pushmenu-open');
+            angular.element(document.body).removeClass('pushmenu-push-toright');
+          }
+        });
 
         $element.toggleClass('active');
         menu.toggleClass('pushmenu-open');
         angular.element(document.body).toggleClass('pushmenu-push-toright');
       });
     }
-  }
+  };
 }])
 
 .directive('pushMenu', [function() {
   return {
     restrict: 'A',
     link: function($scope, $element, $attr) {
-      $element.on('click', function() {
-        var button = angular.element(document.querySelectorAll($attr.originButton));
+      var button = angular.element(document.querySelectorAll($attr.originButton));
 
+      var links = $element.find('a');
+      console.log(links);
+      links.on('click', function() {
         $element.toggleClass('pushmenu-open');
         button.toggleClass('active');
         angular.element(document.body).toggleClass('pushmenu-push-toright');
       });
     }
-  }
+  };
 }]);
